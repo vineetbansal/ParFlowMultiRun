48b383bc95879bcec22c2b978cb55c6442356dba
Lauren Thatch <lmthatch@mines.edu>
2020-09-23 22:57:12 -0600

    updated function docstrings and cleaned comments

--- runScripts/RunParSet.py
+++ runScripts/RunParSet.py
@@ -25,11 +25,19 @@ from ProcessRun import *
     # runSingleFolder, runs a series of simulations, in sequence, in the same folder.
 
 def pfidbGen(runData):              # generates PFIDB File for Run given input data set
-    '''creates pfidb file for parflow run'''
+    '''
+    creates pfidb file for parflow run
+    
+    Args:
+        runData: Pandas DataFrame with Parflow Keys
 
-    # modify input data set
-    runData = runData.drop(labels='n') # remove this values, not needed
-    runData = runData.astype(str)
+    Returns:
+        none
+    '''
+
+    # clean up run data set
+    runData = runData.drop(labels='n') # remove the parameter line file
+    runData = runData.astype(str) # convert all data to strings
 
     # evaluate key and value lengths - these variables are needed for the pfidb file
     keyLengths=[len(i) for i in runData.keys()]
@@ -37,29 +45,44 @@ def pfidbGen(runData):              # generates PFIDB File for Run given input d
 
     # open the output file, then first save the number of keys
     outfn = 'test.pfidb'
-    f = open(outfn,'a')
-    f.write(str(len(runData.keys())))
 
-    # loop through all the keys, add the key len, key, value len, and value to the pfidb file
-    keyVals = np.array(runData)
-    for i in range(len(keyVals)):
-        f.write("\n")
-        f.write(str(keyLengths[i]))
-        f.write("\n")
-        f.write(runData.keys()[i])
-        f.write("\n")
-        if keyVals[i] == 'nan': #nan values should be empty strings
-            f.write("0")
-            f.write("\n")
-            #f.write("\n")
-        else:
-            f.write(str(valueLengths[i]))
-            f.write("\n")
-            f.write(keyVals[i])
-    f.close()
+    with open(outfn,'a') as f: 
 
-def createPumpFile(pumpVRate, pumpDepth_min, pumpDepth_max, dz, nz):
-    '''creates parflow pumping file, given pumping rate (m3/hr), pumping depth range, and DZ/NZ'''
+        # first write out the number of keys to the pfidb file
+        f.write(str(len(runData.keys())))
+
+        # loop through all the keys, add the key length, key, key value length, and key value to the pfidb file
+        keyVals = np.array(runData)
+        for i in range(len(keyVals)):
+            f.write("\n")
+            f.write(str(keyLengths[i]))
+            f.write("\n")
+            f.write(runData.keys()[i])
+            f.write("\n")
+            if keyVals[i] == 'nan': #nan values should be empty strings
+                f.write("0")
+                f.write("\n")
+            else:
+                f.write(str(valueLengths[i]))
+                f.write("\n")
+                f.write(keyVals[i])
+
+def createPumpFile(pumpVRate, pumpDepth_min, pumpDepth_max, dz, dx, nz):
+    '''
+    creates parflow pumping file, given pumping rate (m3/hr), pumping depth range, and DZ/NZ
+    
+    Args:
+        pumpVrate: pumping rate in m3/hr (assumes all model configuraiton in m)
+        pumpDepth_min: minimum pumping depth (i.e. top of screen)
+        pumpDepth_max: maximum pumping depth (i.e bottom of screen)
+        dz: parflow domain layer depth (currently only supports constant dz)
+        dx: parflow domain lateral cell width (assumes dx == dy)
+        nz: Parflow number of vertical layers
+    
+    Returns:
+        none
+    
+    '''
 
     # create pumping vector: for single column this is a simple vector the length of NZ
     pumpData = np.zeros(nz)
@@ -75,7 +98,7 @@ def createPumpFile(pumpVRate, pumpDepth_min, pumpDepth_max, dz, nz):
     nlayers = pumpData.sum()
     pumpDepth = nlayers*dz
     assert pumpDepth == (pumpDepth_max - pumpDepth_min) #sanity check
-    pumpRate = pumpVRate/1.12/1.12/pumpDepth # number of layers does not matter here, only the total depth, because the rate is 1/hr 
+    pumpRate = pumpVRate/dx/dx/pumpDepth # number of layers does not matter here, only the total depth, because the rate is 1/hr 
     pumpData = pumpData * pumpRate
 
     # save the file
@@ -92,7 +115,15 @@ def createPumpFile(pumpVRate, pumpDepth_min, pumpDepth_max, dz, nz):
     os.system('tclsh /glade/scratch/lmthatch/SCTests/runScripts/ConvertPFB2TXT 0 test_pump.txt test_pump.pfb')
 
 def createRunDir(n): #(clmDir,currTestDir) input parameters ignored for now, assuming we're in the current directory and the clm directory has been copied into this folder
-    '''create run directory for parflow files'''
+    '''
+    create folder for parflow run 
+    
+    Args:
+        n: folder number to create
+
+    Returns:
+        none
+    '''
 
     # create run directory
     runDir = 'test' + str(n)
@@ -105,18 +136,44 @@ def createRunDir(n): #(clmDir,currTestDir) input parameters ignored for now, ass
     return runDir
 
 def getInputRow(n,paramFile): # gets the input rows needed.
-    '''get single parameter set from parameter file'''
+    '''
+    get single parameter set from parameter file
+    
+    Args:
+        n: parameter index
+        paramFile: filename (string) with parameters for each parflow run
+    '''
+
     allPar = pd.read_csv(paramFile, delimiter=",", header=0) # room for improvement here, may be removed when 'well' parallelized to loop through parameter set
     linePar = allPar.iloc[n]
     return linePar
 
 def getAllInputRows(paramFile): # gets all input rows
-    '''get all parameter data sets'''
+    '''
+    get all parameter data sets
+    
+    Args:
+        paramFile: filename (string) with parameters for each parflow run
+    '''
+
     allPar = pd.read_csv(paramFile, delimiter=",", header=0) # room for improvement here, may be removed when 'well' parallelized to loop through parameter set
     #linePar = allPar.iloc[n]
     return allPar
 
 def runSet(parLine, parameterFN, parDict):
+    '''
+    run set of parflow parameters and process data
+    runs in it's own folder
+
+    Args:
+        parLine: line in parameter file to read parflow run parameters from
+        parameterFN: parameter file name (str) with parameter data
+        parDict: ParflowMultiRun input parameters 
+    
+    Returns:
+        none
+    '''
+
     #parameterFN = "ParameterSets_AutoGenPY.csv"
     # create your run directory
     newRunDir = createRunDir(parLine)
@@ -139,28 +196,41 @@ def runSet(parLine, parameterFN, parDict):
     os.system('rm test.pfidb')
 
     # delete your folder
-    #os.chdir('../')
-    #os.system('rm ' + newRunDir)
+    os.chdir('../')
+    os.system('rm ' + newRunDir)
 
-    return 'parameter set complete: ' + str(parLine)
+    print('parameter set complete: ' + str(parLine))
 
-def runSingleFolder(runset,parDict): # this is for running in parallel w/ a set number of maximum folders running at a time.
-    # first we'll create a single run folder
+def runSingleFolder(runset,parDict): 
+    '''
+    Runs a full set of parameter lines all in sequences within a single run folder
+
+    Args:
+        runset: set number of parameter files to run
+        parDict: Dictionary w/ ParflowMultiRun Parameters, including processing requirements
+    
+    Returns:
+        none
+    '''
 
     runset = runset - 1 # the GNU parallel is running a sequence from 1-5 but python wants 0-4, correct here.
     print('Running Folder ' + str(runset))
+    
+    # create run directory
     newRunDir = createRunDir(runset)
 
-    parameterFN = "ParameterSets_AutoGenPY_" + str(runset) + ".csv"
-
     # get all run parameters
+    parameterFN = "ParameterSets_AutoGenPY_" + str(runset) + ".csv"
     allPar = getAllInputRows(parameterFN)
-    nsets = len(allPar.index)
+    nsets = len(allPar.index) # number of parameter sets in file
 
+    # move into new run folder
     os.chdir(newRunDir)
 
+    # create file to save run times for each parflow run
     outfn = '../RunTimeData/runTimes_' + str(runset) + '.csv'
 
+    # loop through all parameter sets
     for currset in range(nsets):
         print('Running Set ' + str(currset))
 
@@ -170,8 +240,7 @@ def runSingleFolder(runset,parDict): # this is for running in parallel w/ a set 
         # create your pfidb file
         pfidbGen(runParameters)
 
-        # run your program
-        #os.system("$PARFLOW_DIR/bin/parflow test > parflow.test.log")
+        # run your parflow
         try: 
             start = time.time()
             os.system("$PARFLOW_DIR/bin/parflow test  > parflow.test.log")
@@ -179,17 +248,19 @@ def runSingleFolder(runset,parDict): # this is for running in parallel w/ a set 
 
             # save runtime
             totaltime = end - start
-            f = open(outfn,'a')
             testn = runParameters['n']
-            f.write(str(testn) + "," + str(totaltime))
-            f.write("\n")
-            f.close()
+            with open(outfn,'a') as f:
+                f.write(str(testn) + "," + str(totaltime))
+                f.write("\n")
 
-            print('run complete, processing data')
-            processDataSC(runParameters,parDict)
-
+            try:
+                processDataSC(runParameters,parDict)
+            except:
+                print('parflow processing failed')
+        
         except:
-            'parflow failed'
+            print('parflow failed and/or parflow processing failed')
+        
         os.system('rm test.pfidb')
 
     # delete the directory when all runs have been completed
@@ -204,7 +275,7 @@ def main():
     defaultVals = [True,True,True,True,True,True]
     defaultDict = dict(zip(keys,defaultVals))
 
-    runSingleFolder(runset,defaultVals)
+    runSingleFolder(runset,defaultDict)
 
     # old def main -> When file creation not restricted (no go on Cheyenne)
     # runLine = int(sys.argv[1],"ParameterSets_AutoGenPY.csv")
